2289d373430a4433db5c4b377a5b651866402bff
Tal Zaken <talzaken@gmail.com>
2022-04-12 12:24:50 -0400

    update readme with interface explainer

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-This repo holds files which automatically renew a PythonAnywhere project, 
+## About
+
+This repo holds a module which automatically renews a PythonAnywhere project, 
 so that I don't have to click the button every three months. It also renews itself, which is good.
+
+## How It Works
+
+Automation is powered by Python Selenium, running a headless chrome browser.
+
+The module a really simple interface to navigate the browser, with a function for renewing tasks and a function for renewing web apps:
+
+```python
+# Renews a task (NOTE: only tested on accounts that have a single task configured.)
+renew_task('user_varname', 'password_varname')
+
+# Renews a webapp
+renew_webapp('user_varname', 'password_varname')
+```
+
+Please note that the `'user_varname'` and `'password_varname'` referenced above refer to environment variable names. Pass in the names of environment variables, not the actual credentials to be used.
--- main.py
+++ main.py
@@ -12,15 +12,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-if __name__ == '__main__':
-    # Instantiate browser session with custom options, removing automation flags in chrome
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
+# Instantiate browser session with custom options, removing automation flags in chrome
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--no-sandbox")
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
 
+
+def renew_task(user_varname: str, password_varname: str):
     try:
         driver = webdriver.Chrome(ChromeDriverManager(version="90.0.4430.24").install(), options=options)
     except:
@@ -31,8 +32,32 @@ if __name__ == '__main__':
     username = driver.find_element(by=By.ID, value="id_auth-username")
     pwd = driver.find_element(by=By.ID, value="id_auth-password")
     nxt = driver.find_element(by=By.ID, value="id_next")
-    username.send_keys(os.getenv('PA_username'))
-    pwd.send_keys(os.getenv('PA_password'))
+    username.send_keys(os.getenv(user_varname))
+    pwd.send_keys(os.getenv(password_varname))
+    nxt.click()
+
+    # Navigate to tasks tab
+    driver.get("https://www.pythonanywhere.com/user/talzakenautorenew/tasks_tab/")
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH,
+        "/html/body/div[1]/div[2]/div[2]/div[3]/div/div/table/tbody/tr/td[6]/button[4]"))
+    ).click()
+    driver.close()
+
+
+def renew_webapp(user_varname: str, password_varname: str):
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager(version="90.0.4430.24").install(), options=options)
+    except:
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    # Log into Python Anywhere
+    driver.get("https://www.pythonanywhere.com/login/")
+    username = driver.find_element(by=By.ID, value="id_auth-username")
+    pwd = driver.find_element(by=By.ID, value="id_auth-password")
+    nxt = driver.find_element(by=By.ID, value="id_next")
+    username.send_keys(os.getenv(user_varname))
+    pwd.send_keys(os.getenv(password_varname))
     nxt.click()
 
     # Navigate to web tab
@@ -41,34 +66,10 @@ if __name__ == '__main__':
     # click extend button
     extend_button = driver.find_element(by=By.CLASS_NAME, value='webapp_extend')
     extend_button.click()
-    time.sleep(5)
     driver.close()
 
-    # renew self
-    try:
-        driver = webdriver.Chrome(ChromeDriverManager(version="90.0.4430.24").install(), options=options)
-    except:
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+if __name__ == '__main__':
+    renew_task('PA_renew_username', 'PA_renew_password')
+    renew_webapp('PA_username', 'PA_password')
 
-    # Log into Python Anywhere
-    driver.get("https://www.pythonanywhere.com/login/")
-    username = driver.find_element(by=By.ID, value="id_auth-username")
-    pwd = driver.find_element(by=By.ID, value="id_auth-password")
-    nxt = driver.find_element(by=By.ID, value="id_next")
-    username.send_keys(os.getenv('PA_renew_username'))
-    pwd.send_keys(os.getenv('PA_renew_password'))
-    nxt.click()
-
-
-    # Navigate to tasks tab
-    driver.get("https://www.pythonanywhere.com/user/talzakenautorenew/tasks_tab/")
-    WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH,
-        "/html/body/div[1]/div[2]/div[2]/div[3]/div/div/table/tbody/tr/td[6]/button[4]"))
-    ).click()
-
-    #extend_button = driver.find_element(by=By.CLASS_NAME, value='extend_scheduled_task')
-    #extend_button.click()
-    time.sleep(5)
-    driver.close()
 
